325a0a2b30d05ac0758eaa5ec5109ad65e7440b4
Vitaliy Roschin <kot.official@yandex.ru>
2022-03-13 19:05:23 +0300

    Правки после код-ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,41 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__edit-btn');
-let closePopup  = popup.querySelector('.popup__close-btn');
+let profileEditButton = document.querySelector('.profile__edit-btn');
+let popapCloseButton = popup.querySelector('.popup__close-btn');
 let profileName = document.querySelector('.profile__name');
 let profileAboutMe = document.querySelector('.profile__about-me');
 let formElement = popup.querySelector('.popup__form');
 let inputName = document.getElementById('name');
 let inputAboutMe = document.getElementById('about-me');
-let savePopup = popup.querySelector('.popup__save-btn');
+let popupSaveButton = popup.querySelector('.popup__save-btn');
 
-//Открытие формы редактирования
-openPopup.addEventListener('click', function() {
-  popup.classList.add('popup_opened');
-
+function openPopup() {
   inputName.value = profileName.textContent; //Подставляем имя в форму редактирования
   inputAboutMe.value = profileAboutMe.textContent; //Подставляем информацию "о себе" в форму редактирования
-})
+  popup.classList.add('popup_opened');
+}
 
-//Закрытие формы редактирования на "крестик"
-closePopup.addEventListener('click', function() {
+function closePopup() {
   popup.classList.remove('popup_opened');
-})
+}
 
 //Обработчик «отправки» формы
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value; //Меняем "имя" на странице на значение из формы
   profileAboutMe.textContent = inputAboutMe.value; //Меняем "о себе" на странице на значение из формы
+  closePopup();
 }
+
+//Открытие формы редактирования
+profileEditButton.addEventListener('click', function() {
+  openPopup();
+})
+
+//Закрытие формы редактирования на "крестик"
+popapCloseButton.addEventListener('click', function() {
+  closePopup();
+})
 
 //Меняем данные на странице и закрываем форму кнопкой "Сохранить"
 formElement.addEventListener('submit', formSubmitHandler);
-savePopup.addEventListener('click', function() {
-  popup.classList.remove('popup_opened');
-})
+
