2228f6e3fc9874f43595c3b631e9197c91e58864
Vitaliy Roschin <kot.official@yandex.ru>
2022-03-19 13:34:15 +0300

    feat: load cards from js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-let popup = document.querySelector('.popup');
-let profileEditButton = document.querySelector('.profile__edit-btn');
-let popupCloseButton = popup.querySelector('.popup__close-btn');
-let profileName = document.querySelector('.profile__name');
-let profileAboutMe = document.querySelector('.profile__about-me');
-let formElement = popup.querySelector('.popup__form');
-let inputName = document.getElementById('name');
-let inputAboutMe = document.getElementById('about-me');
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-btn');
+const popupCloseButton = popup.querySelector('.popup__close-btn');
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me');
+const formElement = popup.querySelector('.popup__form');
+const inputName = document.getElementById('name');
+const inputAboutMe = document.getElementById('about-me');
 
 function openPopup() {
   inputName.value = profileName.textContent; //Подставляем имя в форму редактирования
@@ -33,3 +33,51 @@ popupCloseButton.addEventListener('click', closePopup);
 
 //Меняем данные на странице и закрываем форму кнопкой "Сохранить"
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const placesList = document.querySelector('.places__list');
+
+const createCard = (data) => {
+  const placeTemplate = document.querySelector('#place').content;
+  const placeCard = placeTemplate.querySelector('.place').cloneNode(true);
+  const photoCard = placeCard.querySelector('.place__photo');
+  const titleCard = placeCard.querySelector('.place__title');
+  photoCard.src = data.link;
+  titleCard.textContent = data.name;
+  photoCard.alt = data.name;
+  return placeCard;
+}
+
+const renderCard = (data, placesList) => {
+  const placeCard = createCard(data);
+  placesList.prepend(placeCard);
+}
+
+initialCards.forEach(data => { renderCard(data, placesList); });
+
